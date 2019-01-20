476db08426b57725edd8bb949434dc48a7154f57
gsganden <greg@gandenberger.org>
2019-01-19 18:17:11 -0600

    Fix extract path, work on processing for lpz_2016_2017

--- src/data/helpers/datasets.py
+++ src/data/helpers/datasets.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 import logging
 import os
 from pathlib import Path
@@ -34,7 +35,7 @@ class S3Dataset:
         Processor: Type["_Processor"],
         s3_bucket: str,
         s3_key: str,
-        local_dir: str,
+        local_dir: Path,
     ):
         self.local_dir = local_dir
         self.s3_bucket = s3_bucket
@@ -44,7 +45,7 @@ class S3Dataset:
             s3_bucket=self.s3_bucket, s3_key=self.s3_key, local_dir=self.local_dir
         )
         self.extractor = Extractor(archive_path=self.downloader.local_archive_path)
-        self.processor = Processor
+        self.processor = Processor(data_dir=self.local_dir)
 
         self.download = self.downloader.download
         self.extract = self.extractor.extract
@@ -60,7 +61,7 @@ class _Downloader:
 
 
 class S3Downloader(_Downloader):
-    def __init__(self, s3_bucket: str, s3_key: str, local_dir: str) -> None:
+    def __init__(self, s3_bucket: str, s3_key: str, local_dir: Path) -> None:
         self.s3_bucket = s3_bucket
         self.s3_key = s3_key
         self.local_dir = local_dir
@@ -114,25 +115,80 @@ class _Extractor:
 
 
 class TarfileExtractor(_Extractor):
-    def __init__(self, archive_path: str):
+    def __init__(self, archive_path: Path):
         self.archive_path = archive_path
 
     def extract(self):
         with tarfile.open(self.archive_path) as archive:
             members = archive.getmembers()
             for item in tqdm(iterable=members, total=len(members)):
-                archive.extract(member=item)
+                archive.extract(member=item, path=self.local_archive_path.parent)
 
 
 class _Processor:
+    def __init__(self, data_dir: Path):
+        self.data_dir = data_dir
+        self.raw_data_dir = self.data_dir / "raw"
+        self.interim_data_dir = self.raw_data_dir.parent / "interim"
+        self.processed_data_dir = self.raw_data_dir.parent / "processed"
+
     def process(self):
+        self.process_images()
+        self.process_labels()
+
+    def process_images(self):
         raise NotImplementedError
+
+    def process_labels(self):
+        raise NotImplementedError
+
+
+class LPZ_2016_2017_Processor(_Processor):
+    def process(self, local_dir: Path):
+        super().__init__(local_dir)
+        self.process_images()
+        self.process_labels()
+
+    def process_images(self):
+        pass
+
+    def process_labels(self):
+        raise NotImplementedError
+
+        labels = self.load_label_dataframes()
+        self._correct_filepaths(df=labels, image_dir=self.raw_data_dir / "images")
+        discard_rows_where_file_is_missing(df=labels, path_col="filepath")
+        labels = _get_dummies(labels, columns=["ShortName"])
+        discard_duplicate_rows(labels)
+        labels = _groupby(df=labels, field="filepath", aggregation_func=max)
+        _add_image_properties(labels, image_properties)
+        labels = _clean_label_columns(labels)
+
+
+class LPZ_2016_2017_RawProcessor(LPZ_2016_2017_Processor):
+    def process_images(self):
+        raise NotImplementedError
+
+        image_properties = defaultdict(list)
+        image_paths = get_paths_with_extensions(self.raw_data_dir, extensions)
+        for path in tqdm(image_paths):
+            image = cv.imread(str(path))
+            try:
+                processed_image = _process_image(image)
+            except (ZeroDivisionError, TypeError):
+                logging.warning(f"Skipping {path}, which did not load properly.")
+            else:
+                outpath = abspath(path).replace(abspath(indir), abspath(outdir))
+                _record_properties(processed_image, image_properties, path, outpath)
+                save_image(processed_image, outpath)
+        csv_outpath = Path(outdir) / "image_properties.csv"
+        write_csv(pd.DataFrame(image_properties), csv_outpath)
 
 
 lpz_data_2016_2017_raw = S3Dataset(
     S3Downloader=S3Downloader,
     Extractor=TarfileExtractor,
-    Processor=_Processor,
+    Processor=LPZ_2016_2017_RawProcessor,
     s3_bucket="autofocus",
     s3_key="lpz_data/data_2016_2017.tar.gz",
     local_dir=DATA_DIR / "lpz",
