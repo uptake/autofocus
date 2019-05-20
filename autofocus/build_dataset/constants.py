9c21c5ec24a7192e10f20e6eb80f5547ba5d80c9
gsganden <gsganden@gmail.com>
2019-05-19 21:14:14 -0500

    Add non-black code as a test

--- autofocus/build_dataset/constants.py
+++ autofocus/build_dataset/constants.py
@@ -2,5 +2,5 @@ from pathlib import Path  # noqa: D100
 from typing import Union
 
 REPO_DIR = Path(__file__).resolve().parents[2]
-DATA_DIR = REPO_DIR / "data"
+DATA_DIR = REPO_DIR/"data"
 PathOrStr = Union[Path, str]
