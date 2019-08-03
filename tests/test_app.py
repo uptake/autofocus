c37a0e283b4bc1bf1ea020b0a20e49b7c759988a
gsganden <gsganden@gmail.com>
2019-08-03 12:25:59 -0500

    Test zip endpoint too

--- tests/test_app.py
+++ tests/test_app.py
@@ -17,3 +17,11 @@ def test_sample_predict_request_JPG():
         "http://0.0.0.0:8000/predict", files={"file": open(filepath, "rb")}
     )
     assert response.json()["deer"] > 0.9
+
+
+def test_sample_predict_zip_request():
+    filepath = Path(__file__).resolve().parents[1] / "gallery.zip"
+    response = requests.post(
+        "http://0.0.0.0:8000/predict_zip", files={"file": open(filepath, "rb")}
+    )
+    assert len(response.json()) == 5
